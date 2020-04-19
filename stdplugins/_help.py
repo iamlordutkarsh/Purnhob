f1fc2c3e3c3496d520b6e6be3a3bbb2a2f8efc58
iamlordutkarsh <48874181+iamlordutkarsh@users.noreply.github.com>
2020-04-19 18:36:56 +0530

    Fixes

--- stdplugins/_help.py
+++ stdplugins/_help.py
@@ -14,11 +14,11 @@ async def _(event):
         s_help_string = borg._plugins[splugin_name].__doc__
     else:
         s_help_string = "****:"
-    help_string = """@UniBorg ( **Custom Built By** @r4v4n4 ) \n**Verified Account**: ✅\n**Official Website**: https://ravanaisdrunk.site.live\n
+    help_string = """@UniBorg ( **Custom Built By** @lordwitcher ) \n**Verified Account**: ✅\n**Official Website**: https://developerutkarsh.wordpress.com\n
 Pithun {}
 Talethrun {}
  
-**Custom Built Fork**: https://github.com/ravana69/PornHub""".format(
+**Custom Built Fork**: https://github.com/iamlordutkarsh/PurnHob""".format(
         sys.version,
         __version__
     )
