f95ff6a388007cc1a460ecad5fdf27ee75eb960c
iamlordutkarsh <48874181+iamlordutkarsh@users.noreply.github.com>
2020-06-06 23:34:33 +0530

    Update alive.py

--- stdplugins/alive.py
+++ stdplugins/alive.py
@@ -4,12 +4,18 @@ from telethon import events
 from telethon.tl.types import ChannelParticipantsAdmins
 from uniborg.util import admin_cmd
 
+###additional stuff 
+
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else uname().node
+
+######
+
 
 @borg.on(admin_cmd("alive"))
 async def _(event):
     if event.fwd_from:
         return
-    mentions = " User: @lordwitcher \n Server:Online \n Telethon version: 1.11.3 \n Python: 3.8.2 "
+    mentions = " Working Fine Keks \n My Owner: {DEFAULTUSER} \n Server:Online \n Telethon version: 1.11.3 \n Python: 3.8.3 "
     chat = await event.get_input_chat()
     async for x in borg.iter_participants(chat, filter=ChannelParticipantsAdmins):
         mentions += f""
